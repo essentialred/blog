adf7b472911b4b5fdfa35af82afc85940a9c0e56
Megan Tong <meganhotong@gmail.com>
2015-01-30 16:37:12 -0500

    no more browserify

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,16 +17,9 @@ module.exports = function(grunt) {
 			purecss: 'assets/less/pages/purecss.less',
 			cssWithJs: 'assets/less/pages/css-with-js.less',
 			noBgSize: 'assets/less/pages/no-bg-size.less'
-		},
-
-		js: {
-			cssWithJs: 'assets/js/src/css-with-js.js',
-			noBgSize: 'assets/js/src/no-bg-size.js'
 		}
 
 	};
-
-	var vendorExternal = ['jquery', 'jquery-scrollTie'];
 
 	grunt.initConfig({
 		
@@ -49,28 +42,11 @@ module.exports = function(grunt) {
 			}
 		},
 
-		browserify: {
-
-			src: {
-				files: {
-					'assets/js/src.bundle.js': [config.paths.src + 'main.js'],
-					'assets/js/cssWithJs.bundle.js' : [config.js.cssWithJs],
-					'assets/js/noBgSize.bundle.js' : [config.js.noBgSize]
-				}
-			}
-
-		},
-
 		watch: {
 
 			less: {
 				files: [config.paths.built + 'less/**/*.less'],
 				tasks: ['less']
-			},
-
-			browserify: {
-				files: [config.paths.src + '**/*.js'],
-				tasks: ['browserify:src']
 			},
 
 			jshint: {
@@ -81,16 +57,11 @@ module.exports = function(grunt) {
 				tasks: ['jshint']
 			},
 
-			concat: {
-				files: ['assets/js/vendor.bundle.js', 'assets/js/src.bundle.js'],
-				tasks: ['concat']
-			},
-
 			livereload: {
 				options: {
 					livereload: true
 				},
-				files: [config.paths.built + 'css/**/*.css', 'assets/js/bundle.js', 'index.php', 'partials/*.php']
+				files: [config.paths.built + 'css/**/*.css', '*.html']
 			}
 		},
 
@@ -113,12 +84,10 @@ module.exports = function(grunt) {
 	});
 
 	// load NPM tasks
-	grunt.loadNpmTasks('grunt-browserify');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-concat');
 
 	// register custom tasks
-	grunt.registerTask('default', ['less', 'browserify']);
+	grunt.registerTask('default', ['less']);
 };
